1dcfb60e068facbbee6377791a69136221c7a3ac
Carlos Eduardo Toni de Azevedo <80012970+carlostoni@users.noreply.github.com>
2023-09-23 17:19:04 -0300

    Add files via upload

---
+++ ATIVIDADE.py
@@ -0,0 +1,15 @@
+print("Digite os numeros a serem somados")
+n = float(input("Digite primeiro numero "))
+n2 = float(input("Digite segundo numero "))
+n3 =  n + n2
+print("O resultado é:", n3)
+
+nome = input("Digite o seu nome:")
+print(nome)
+
+print(nome,"Parabens")
+
+n = int(input("Digite primeiro numero "))
+n2 = int(input("Digite segundo numero "))
+n3 =  n * n2
+print("O resultado é:", n3)
