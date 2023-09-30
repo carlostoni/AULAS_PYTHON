8f98ac2e663703aaebcba9dbd71fd5f7aca638d2
Carlos Eduardo Toni de Azevedo <80012970+carlostoni@users.noreply.github.com>
2023-09-30 17:49:21 -0300

    Add files via upload

---
+++ Aula_4/AULA_4.py
@@ -0,0 +1,34 @@
+# age = int(input("qual sua idade:"))
+# if age >=18 and age <=65:
+#     print("Voce ja esta na idade de votar")
+# else:
+#     print("Voce nao pode votar")
+
+# if not age != 25:
+#     print("E diferente de 25")
+# else:
+#     print("sssdsdsd")
+
+# language  =  "Inglês"    
+
+# if not language  == "Português":
+#    print("Contratado ")
+# else: 
+#    print("Não selecionado")
+# var1 = 123
+# var2 = "World"
+# print("Hello to the {} {}" .format(var2,var1))
+
+# nota = 10
+# if nota == 10:
+#     print("Voce Passou de ano")
+# elif nota >= 7 and nota <=9:
+#     print("Voce foi bem")
+# else:
+#     print("Deixa para a proxima")
+
+numero = 5
+if numero % 2 !=0:
+    print("O numero e impar")
+else:
+    print("o numero e par")
