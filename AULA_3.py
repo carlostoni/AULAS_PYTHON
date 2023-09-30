fb5bf8c6f4fecc0969f15c5e2704123706232e65
Carlos Eduardo Toni de Azevedo <80012970+carlostoni@users.noreply.github.com>
2023-09-30 16:22:15 -0300

    Class 3 content

---
+++ AULA_3.py
@@ -0,0 +1,52 @@
+# senha =  1234
+# entrada  = int(input("Digite sua senha"))
+
+
+# if (entrada == 1234):
+#   print('Acesso permitido')
+# else: 
+#     print('Acesso negado')
+
+#formart
+
+# nome = input("Digite o seu nome - ")
+# idade = input("Digite aqui sua idade - ")
+# meu_endereco = input("Digite se endereço - ")
+# cargo = input("Digite aqui o seu cargo - ")
+
+# print("Meu nome é,",nome, "minha idade é, ",idade,"meu endereço é, ",meu_endereco, "meu cargo é, ",cargo)
+
+# print(f"Meu nome é {nome}, minha idade é, {idade}, meu endereço é {meu_endereco}, meu cargo é {cargo}")
+
+#Condicionais
+# senha_digitada = input("Digite uma senha")
+# senha  = "1234"
+
+# if senha == senha_digitada:
+#     print("Acesse")
+# else:
+#     print("Errada")
+
+# senha_digitada = input("Digite uma senha")
+# senha  = "1234"
+# nome = input("Digite o seu nome")
+
+# if senha == senha_digitada and nome == "Camila":
+#     print("Acesse")
+# else:
+#     print("Errada")
+
+# senha_digitada = input("Digite uma senha")
+# senha  = "1234"
+# nome = input("Digite o seu nome")
+
+# if senha == senha_digitada or nome == "Camila":
+#     print("Acesse")
+# else:
+#     print("Errada")
+class Operacao:
+  def condicao():
+    if 2 == 2:
+      print("É igual")
+    else:
+      print("É diferente")
