62fcbd4943d95d1cd080d000a4bbe91920674639
Carlos Eduardo Toni de Azevedo <80012970+carlostoni@users.noreply.github.com>
2023-10-07 15:23:15 -0300

    AULA_6

---
+++ AULA_6/AULA_6_ATIVIDADE.py
@@ -0,0 +1,34 @@
+## ATIVIDADE 1
+# numeros = list[1,2,3,4,5]
+# print(numeros)
+
+## ATIVIDADE 2
+# numeros = [1,2,3,4,5]
+# print(numeros[2])
+
+## ATIVIDADE 3
+# numeros = [1,2,3,4,5]
+# numeros.append(6)
+# print(numeros)
+
+## ATIVIDADE 4
+# numeros = [1,2,3,4,5]
+# numeros.remove(3)
+# print(numeros)
+
+## ATIVIDADE 5
+frutas = ['maça','banana','uva']
+numeros = [1,2,3]
+print(f'{frutas[0]} - {numeros[0]}, {frutas[1]} - {numeros[1]}, {frutas[2]} - {numeros[2]}')
+
+## ATIVIDADE 6
+# frutas = ['maça','banana','uva']
+# for i in frutas:
+#     print(i)
+
+## ATIVIDADE 7
+# numeros = [1,2,3,4,5]
+# if 5 in numeros:
+#     print(f'O Numero {numeros[4]}')
+# else:
+#     print('Não esta na lista ')
---
+++ AULA_6/AULA_6_RESOLUCAO.py
@@ -0,0 +1,29 @@
+# ## ATIVIDADE 1
+# lista = [1,2,3,4,5]
+# print(lista)
+
+# ## ATIVIDADE 2
+# x = lista[2]
+
+# ## ATIVIDADE 3
+# lista = [1,2,3,4,5]
+# lista.append(6)
+# print(lista)
+
+# ##ATIVIDADE 4
+# lista = [1,2,3,4,5]
+# lista.remove(3)
+# print(lista)
+
+## ATIVIDADE 5
+# frutas = ['maça','melão','uva']
+# numeros = [1,2,3]
+# juntado = frutas + numeros
+# print(juntado)
+
+## ATIVIDADE 6
+# lista = [1,2,3,4,5]
+# if 5 in lista:
+#     print(f'o numero esta presente{lista[4]}')
+# else:
+#      print('Não esta na lista ')
