6583ebe6b8f67c9667b8ea76dcc49b4af6eba6dd
Carlos Eduardo Toni de Azevedo <80012970+carlostoni@users.noreply.github.com>
2023-12-16 12:32:20 -0300

    Update x.py

--- AULA_25/x.py
+++ AULA_25/x.py
@@ -114,3 +114,64 @@ delete_button = tk.Button(root, text="Delete", command=delete)
 delete_button.pack()
 
 root.mainloop()
+
+import tkinter as tk
+from tkinter import messagebox
+
+def  create():
+    item = item_entry.get()
+    if item:
+        items_listbox.insert(tk.END, item)
+        item_entry.delete(0, tk.END)
+
+def read():
+    selected_item = items_listbox.curselection()
+    if selected_item:
+        item = items_listbox.get(selected_item)
+        messagebox.showinfo('Selecionado', f'Dados - {item}')
+   
+
+
+def update():
+    selected_item = items_listbox.curselection()
+    if selected_item:
+        new_item = item_entry.get()
+        if new_item:
+            items_listbox.delete(selected_item)
+            items_listbox.insert(selected_item, new_item)
+            item_entry.delete(0, tk.END)
+
+def delete():
+    selected_item = items_listbox.curselection()
+    if selected_item:
+        items_listbox.delete(selected_item)
+
+root = tk.Tk()
+root.title('CRUD')
+root.geometry('400x400')
+
+
+
+item_label = tk.Label(root, text= 'Digite o seu E- mail', font=('arial',25, 'bold'))
+item_label.pack()
+item_entry = tk.Entry(root, width=50)
+item_entry.pack()
+
+btn_create = tk.Button(root, text='Salvar', command=create, width=15)
+btn_create.pack()
+
+items_listbox = tk.Listbox(root, width=50)
+items_listbox.pack()
+
+btn_read = tk.Button(root, text= 'Ler', command=read, width=15)
+btn_read.pack()
+
+btn_update = tk.Button(root, text ='Atualizar', command=update, width=15)
+btn_update.pack()
+
+btn_delete = tk.Button(root, text= 'Deletar', command=delete, width=15)
+btn_delete.pack()
+
+
+
+root.mainloop()
